7e297bcf7479d55855f394b092d9628aaf97f4d8
sopa <88498914+HenriqueSoubhia@users.noreply.github.com>
2022-11-01 12:10:23 -0300

    Add files via upload

--- routes/feed.js
+++ routes/feed.js
@@ -1,19 +1,31 @@
-module.exports = (app)=> {
+module.exports = (app) => {
 
     let conexao = require('../config/database')
 
-    app.get('/feed', async(req,res)=> {
+    app.get('/feed', async (req, res) => {
         conexao()
 
         let user = req.query.id
 
         let postings = require("../models/posting")
 
-        let posts = await postings.find().sort({'_id':-1})
+        let posts = await postings.find().sort({
+            '_id': -1
+        })
 
-        console.log(posts),
+        const qty = posts.length / 2
+        const chunks = posts.reduce((array, item, index) => {
+            const chunkIndex = Math.floor(index / qty)
+            if (!array[chunkIndex]) {
+                array[chunkIndex] = []
+            }
+            array[chunkIndex].push(item)
+            return array
+        }, [])
 
-
-        res.render('feed',{id:user, posts:posts})
+        res.render('feed', {
+            id: user,
+            chunks: chunks
+        })
     })
 }
